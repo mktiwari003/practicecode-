e774647ddcf48b0b9fd11caa0529c00efb1ec127
manish <manish.tiwari@infostretch.com>
2019-12-22 13:56:50 +0530

    maven commit

---
+++ sel_three/apoint_workspace/automation-bd/src1/com/automation/bd/automation_bd/AppTest1.java
@@ -0,0 +1,24 @@
+package com.automation.bd.automation_bd;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest1 {
+    
+    
+	
+   
+    
+    @Test()
+    public void AppTest12( ) {
+        System.out.println("working with maven project AppTest12");
+    }
+
+    @Test()
+    public void AppTest13( ) {
+        System.out.println("working with maven project AppTest13");
+    }
+   
+}
